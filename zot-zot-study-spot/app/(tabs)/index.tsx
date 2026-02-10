bc840430d6cdeebf9778028ab7c816e108c5d6ee
shaileyjoseph <shaileyjoseph2006@gmail.com>
2026-02-09 21:13:20 -0800

    map now shows user location

--- zot-zot-study-spot/app/(tabs)/index.tsx
+++ zot-zot-study-spot/app/(tabs)/index.tsx
@@ -1,26 +1,15 @@
 
 import { Image } from "expo-image";
 import { View, StyleSheet, Pressable } from "react-native";
-import React, { useState } from "react";
+import React, {  useEffect, useState } from "react";
 import { FilterBar, Filters } from "@/components/filter-bar";
 import { FontAwesome6 } from "@expo/vector-icons";
 import MapView, { Geojson, Marker } from "react-native-maps";
 import { Colors, Brand, Fonts } from "@/constants/theme";
 import FeatureCollection, {GeoJsonObject} from 'geojson';
+//import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import * as Location from "expo-location";
 
-const myPlace = {
-  type: 'FeatureCollection',
-  features: [
-    {
-      type: 'Feature',
-      properties: {},
-      geometry: {
-        type: 'Point',
-        coordinates: [-117.84279, 33.64579],
-      }
-    }
-  ]
-};
 const route = {"type":"FeatureCollection","bbox":[-117.846031,33.644952,-117.842622,33.645906],"features":[{"bbox":[-117.846031,33.644952,-117.842622,33.645906],"type":"Feature","properties":{"segments":[{"distance":397.7,"duration":286.3,"steps":[{"distance":22.4,"duration":16.1,"type":11,"instruction":"Head northwest","name":"-","way_points":[0,1]},{"distance":30,"duration":21.6,"type":5,"instruction":"Turn slight right","name":"-","way_points":[1,2]},{"distance":22.8,"duration":16.4,"type":12,"instruction":"Keep left","name":"-","way_points":[2,3]},{"distance":82.1,"duration":59.1,"type":4,"instruction":"Turn slight left","name":"-","way_points":[3,7]},{"distance":112.2,"duration":80.8,"type":12,"instruction":"Keep left","name":"-","way_points":[7,14]},{"distance":128.2,"duration":92.3,"type":0,"instruction":"Turn left","name":"-","way_points":[14,24]},{"distance":0,"duration":0,"type":10,"instruction":"Arrive at your destination, straight ahead","name":"-","way_points":[24,24]}]}],"way_points":[0,24],"summary":{"distance":397.7,"duration":286.3}},"geometry":{"coordinates":[[-117.842622,33.644952],[-117.842742,33.645123],[-117.842699,33.645391],[-117.842776,33.645586],[-117.843101,33.645767],[-117.843289,33.645801],[-117.843312,33.645805],[-117.84358,33.645869],[-117.843659,33.645836],[-117.843954,33.645821],[-117.844256,33.645796],[-117.844492,33.645789],[-117.84459,33.645786],[-117.84464,33.645789],[-117.844647,33.645906],[-117.844734,33.645904],[-117.844888,33.645899],[-117.844976,33.645897],[-117.845281,33.645896],[-117.845353,33.645896],[-117.845499,33.645896],[-117.845686,33.645888],[-117.845928,33.645879],[-117.846024,33.645876],[-117.846031,33.645876]],"type":"LineString"}}],"metadata":{"attribution":"openrouteservice.org | OpenStreetMap contributors","service":"routing","timestamp":1770326786099,"query":{"coordinates":[[-117.842301,33.645109],[-117.846031,33.645871]],"profile":"foot-walking","profileName":"foot-walking","format":"json"},"engine":{"version":"9.5.0","build_date":"2025-10-31T12:33:09Z","graph_date":"2026-01-26T15:03:31Z","osm_date":"2026-01-19T01:00:01Z"}}}
 
 export default function HomeScreen() {
@@ -29,24 +18,37 @@ export default function HomeScreen() {
 		environment: "any",
 		techEnhanced: false,
 	});
-
+	
 	const theme = Colors.light;
+	
+	useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        return;
+      }
 
+      let location = await Location.getCurrentPositionAsync({});
+    }
+
+    getCurrentLocation();
+  }, []);
+	
 	return (
 		<View style={[styles.container, { backgroundColor: theme.background }]}>
+
 			{/* Map */}
 			<MapView style={styles.image}
 				initialRegion={{
 					latitude: 33.64579,
           			longitude: -117.84279,
           			latitudeDelta: 0.008,
-          			longitudeDelta: 0.008,}}>
+          			longitudeDelta: 0.008,}}
+					showsUserLocation>
 				<Geojson
       				geojson={route}
-      				//strokeColor="red"
-      				//fillColor="green"
-      				//strokeWidth={2}
     			/>
+				
 			</MapView>
 
 			{/* Floating filter bar */}
@@ -125,3 +127,14 @@ const styles = StyleSheet.create({
 		shadowRadius: 10,
 	},
 });
+
+const getLocationPermission = async() =>
+{
+	var status = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+	switch (status) {
+    case RESULTS.DENIED:
+      await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    case RESULTS.GRANTED:
+      return
+}
+}
