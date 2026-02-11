7ddd37c8cc08a41e7b4460a3892c2fdb0c606326
shaileyjoseph <shaileyjoseph2006@gmail.com>
2026-02-10 18:29:20 -0800

    created initial marker events, removed apple markers

--- zot-zot-study-spot/app/(tabs)/index.tsx
+++ zot-zot-study-spot/app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { View, StyleSheet, Pressable } from "react-native";
 import React, {  useEffect, useState } from "react";
 import { FilterBar, Filters } from "@/components/filter-bar";
 import { FontAwesome6 } from "@expo/vector-icons";
-import MapView, { Geojson, Marker } from "react-native-maps";
+import MapView, { LatLng, Geojson, Marker} from "react-native-maps";
 import { Colors, Brand, Fonts } from "@/constants/theme";
 import FeatureCollection, {GeoJsonObject} from 'geojson';
 //import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
@@ -12,6 +12,7 @@ import * as Location from "expo-location";
 
 const route = {"type":"FeatureCollection","bbox":[-117.846031,33.644952,-117.842622,33.645906],"features":[{"bbox":[-117.846031,33.644952,-117.842622,33.645906],"type":"Feature","properties":{"segments":[{"distance":397.7,"duration":286.3,"steps":[{"distance":22.4,"duration":16.1,"type":11,"instruction":"Head northwest","name":"-","way_points":[0,1]},{"distance":30,"duration":21.6,"type":5,"instruction":"Turn slight right","name":"-","way_points":[1,2]},{"distance":22.8,"duration":16.4,"type":12,"instruction":"Keep left","name":"-","way_points":[2,3]},{"distance":82.1,"duration":59.1,"type":4,"instruction":"Turn slight left","name":"-","way_points":[3,7]},{"distance":112.2,"duration":80.8,"type":12,"instruction":"Keep left","name":"-","way_points":[7,14]},{"distance":128.2,"duration":92.3,"type":0,"instruction":"Turn left","name":"-","way_points":[14,24]},{"distance":0,"duration":0,"type":10,"instruction":"Arrive at your destination, straight ahead","name":"-","way_points":[24,24]}]}],"way_points":[0,24],"summary":{"distance":397.7,"duration":286.3}},"geometry":{"coordinates":[[-117.842622,33.644952],[-117.842742,33.645123],[-117.842699,33.645391],[-117.842776,33.645586],[-117.843101,33.645767],[-117.843289,33.645801],[-117.843312,33.645805],[-117.84358,33.645869],[-117.843659,33.645836],[-117.843954,33.645821],[-117.844256,33.645796],[-117.844492,33.645789],[-117.84459,33.645786],[-117.84464,33.645789],[-117.844647,33.645906],[-117.844734,33.645904],[-117.844888,33.645899],[-117.844976,33.645897],[-117.845281,33.645896],[-117.845353,33.645896],[-117.845499,33.645896],[-117.845686,33.645888],[-117.845928,33.645879],[-117.846024,33.645876],[-117.846031,33.645876]],"type":"LineString"}}],"metadata":{"attribution":"openrouteservice.org | OpenStreetMap contributors","service":"routing","timestamp":1770326786099,"query":{"coordinates":[[-117.842301,33.645109],[-117.846031,33.645871]],"profile":"foot-walking","profileName":"foot-walking","format":"json"},"engine":{"version":"9.5.0","build_date":"2025-10-31T12:33:09Z","graph_date":"2026-01-26T15:03:31Z","osm_date":"2026-01-19T01:00:01Z"}}}
 
+//put <Geojson geojson={route} /> inside mapview to show a route
 export default function HomeScreen() {
 	const [filters, setFilters] = useState<Filters>({
 		capacity: null,
@@ -30,24 +31,33 @@ export default function HomeScreen() {
 
       let location = await Location.getCurrentPositionAsync({});
     }
-
-    getCurrentLocation();
-  }, []);
+  	}, []);
 	
 	return (
 		<View style={[styles.container, { backgroundColor: theme.background }]}>
 
 			{/* Map */}
-			<MapView style={styles.image}
+			<MapView style={styles.image} 
+				//events
+				onUserLocationChange={update_route_as_user_moves}
+
+				//non-events
+				showsPointsOfInterest = {false} //removes default apple pins since ours overlap
+				showsUserLocation
 				initialRegion={{
 					latitude: 33.64579,
           			longitude: -117.84279,
           			latitudeDelta: 0.008,
-          			longitudeDelta: 0.008,}}
-					showsUserLocation>
-				<Geojson
-      				geojson={route}
-    			/>
+          			longitudeDelta: 0.008,}}>
+
+				<Marker
+					//events
+					onSelect = {select_marker}
+					onDeselect = {deselect_marker}
+					
+					//characteristics
+					coordinate = {get_marker_coordinates()}
+				/>
 				
 			</MapView>
 
@@ -79,6 +89,27 @@ export default function HomeScreen() {
 		</View>
 	);
 }
+
+function select_marker()
+{
+	return
+}
+
+function deselect_marker()
+{
+	return
+}
+
+function update_route_as_user_moves()
+{
+	return
+}
+
+function get_marker_coordinates()
+{
+	return {latitude: 33.64579, longitude: -117.84279};
+}
+
 
 const styles = StyleSheet.create({
 	container: { flex: 1 },
@@ -127,14 +158,3 @@ const styles = StyleSheet.create({
 		shadowRadius: 10,
 	},
 });
-
-const getLocationPermission = async() =>
-{
-	var status = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-	switch (status) {
-    case RESULTS.DENIED:
-      await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-    case RESULTS.GRANTED:
-      return
-}
-}
