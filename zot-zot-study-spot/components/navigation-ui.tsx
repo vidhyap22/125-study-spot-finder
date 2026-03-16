65e9dd33d2707b5dccab575feb5cf6cefc4b87b1
shaileyjoseph <shaileyjoseph2006@gmail.com>
2026-03-16 11:31:31 -0700

    added requirements file

--- zot-zot-study-spot/components/navigation-ui.tsx
+++ zot-zot-study-spot/components/navigation-ui.tsx
@@ -6,78 +6,170 @@ import {StyleSheet, Pressable, View, Text} from "react-native";
 import { BottomSheetPanel } from "@/components/bottom-sheet-panel";
 
 export const ex_route = {
-	type: "FeatureCollection",
-	bbox: [-117.846031, 33.644952, -117.842622, 33.645906],
-	features: [
-		{
-			bbox: [-117.846031, 33.644952, -117.842622, 33.645906],
-			type: "Feature",
-			properties: {
-				segments: [
-					{
-						distance: 397.7,
-						duration: 286.3,
-						steps: [
-							{ distance: 22.4, duration: 16.1, type: 11, instruction: "Head northwest", name: "-", way_points: [0, 1] },
-							{ distance: 30, duration: 21.6, type: 5, instruction: "Turn slight right", name: "-", way_points: [1, 2] },
-							{ distance: 22.8, duration: 16.4, type: 12, instruction: "Keep left", name: "-", way_points: [2, 3] },
-							{ distance: 82.1, duration: 59.1, type: 4, instruction: "Turn slight left", name: "-", way_points: [3, 7] },
-							{ distance: 112.2, duration: 80.8, type: 12, instruction: "Keep left", name: "-", way_points: [7, 14] },
-							{ distance: 128.2, duration: 92.3, type: 0, instruction: "Turn left", name: "-", way_points: [14, 24] },
-							{ distance: 0, duration: 0, type: 10, instruction: "Arrive at your destination, straight ahead", name: "-", way_points: [24, 24] },
-						],
-					},
-				],
-				way_points: [0, 24],
-				summary: { distance: 397.7, duration: 286.3 },
-			},
-			geometry: {
-				coordinates: [
-					[-117.842622, 33.644952],
-					[-117.842742, 33.645123],
-					[-117.842699, 33.645391],
-					[-117.842776, 33.645586],
-					[-117.843101, 33.645767],
-					[-117.843289, 33.645801],
-					[-117.843312, 33.645805],
-					[-117.84358, 33.645869],
-					[-117.843659, 33.645836],
-					[-117.843954, 33.645821],
-					[-117.844256, 33.645796],
-					[-117.844492, 33.645789],
-					[-117.84459, 33.645786],
-					[-117.84464, 33.645789],
-					[-117.844647, 33.645906],
-					[-117.844734, 33.645904],
-					[-117.844888, 33.645899],
-					[-117.844976, 33.645897],
-					[-117.845281, 33.645896],
-					[-117.845353, 33.645896],
-					[-117.845499, 33.645896],
-					[-117.845686, 33.645888],
-					[-117.845928, 33.645879],
-					[-117.846024, 33.645876],
-					[-117.846031, 33.645876],
-				],
-				type: "LineString",
-			},
-		},
-	],
-	metadata: {
-		attribution: "openrouteservice.org | OpenStreetMap contributors",
-		service: "routing",
-		timestamp: 1770326786099,
-		query: {
-			coordinates: [
-				[-117.842301, 33.645109],
-				[-117.846031, 33.645871],
-			],
-			profile: "foot-walking",
-			profileName: "foot-walking",
-			format: "json",
-		},
-		engine: { version: "9.5.0", build_date: "2025-10-31T12:33:09Z", graph_date: "2026-01-26T15:03:31Z", osm_date: "2026-01-19T01:00:01Z" },
-	},
+  "type": "FeatureCollection",
+  "bbox": [-117.841848, 33.644486, -117.8412, 33.647261],
+  "features": [
+    {
+      "bbox": [-117.841848, 33.644486, -117.8412, 33.647261],
+      "type": "Feature",
+      "properties": {
+        "segments": [
+          {
+            "distance": 383.2,
+            "duration": 275.9,
+            "steps": [
+              {
+                "distance": 15.5,
+                "duration": 11.2,
+                "type": 11,
+                "instruction": "Head northeast",
+                "name": "-",
+                "way_points": [0, 2]
+              },
+              {
+                "distance": 20.6,
+                "duration": 14.8,
+                "type": 0,
+                "instruction": "Turn left",
+                "name": "-",
+                "way_points": [2, 4]
+              },
+              {
+                "distance": 8.1,
+                "duration": 5.8,
+                "type": 0,
+                "instruction": "Turn left",
+                "name": "-",
+                "way_points": [4, 5]
+              },
+              {
+                "distance": 48.1,
+                "duration": 34.6,
+                "type": 1,
+                "instruction": "Turn right",
+                "name": "-",
+                "way_points": [5, 9]
+              },
+              {
+                "distance": 193.5,
+                "duration": 139.3,
+                "type": 1,
+                "instruction": "Turn right onto Inner Ring",
+                "name": "Inner Ring",
+                "way_points": [9, 17]
+              },
+              {
+                "distance": 32.3,
+                "duration": 23.3,
+                "type": 1,
+                "instruction": "Turn right",
+                "name": "-",
+                "way_points": [17, 21]
+              },
+              {
+                "distance": 31.7,
+                "duration": 22.8,
+                "type": 1,
+                "instruction": "Turn right",
+                "name": "-",
+                "way_points": [21, 23]
+              },
+              {
+                "distance": 9.8,
+                "duration": 7.1,
+                "type": 1,
+                "instruction": "Turn right",
+                "name": "-",
+                "way_points": [23, 24]
+              },
+              {
+                "distance": 17.8,
+                "duration": 12.8,
+                "type": 1,
+                "instruction": "Turn right",
+                "name": "-",
+                "way_points": [24, 26]
+              },
+              {
+                "distance": 5.7,
+                "duration": 4.1,
+                "type": 1,
+                "instruction": "Turn right",
+                "name": "-",
+                "way_points": [26, 27]
+              },
+              {
+                "distance": 0,
+                "duration": 0,
+                "type": 10,
+                "instruction": "Arrive at your destination, on the left",
+                "name": "-",
+                "way_points": [27, 27]
+              }
+            ]
+          }
+        ],
+        "way_points": [0, 27],
+        "summary": {
+          "distance": 383.2,
+          "duration": 275.9
+        }
+      },
+      "geometry": {
+        "coordinates": [
+          [-117.841848, 33.644486],
+          [-117.841793, 33.644521],
+          [-117.841715, 33.644571],
+          [-117.84173, 33.644588],
+          [-117.841573, 33.644687],
+          [-117.841626, 33.644745],
+          [-117.841538, 33.644816],
+          [-117.841459, 33.64494],
+          [-117.841471, 33.645037],
+          [-117.841528, 33.645117],
+          [-117.841411, 33.64525],
+          [-117.84132, 33.645406],
+          [-117.841241, 33.645581],
+          [-117.841201, 33.64573],
+          [-117.8412, 33.645968],
+          [-117.841254, 33.646196],
+          [-117.841358, 33.646446],
+          [-117.841562, 33.646726],
+          [-117.841516, 33.646775],
+          [-117.841514, 33.646872],
+          [-117.841531, 33.646913],
+          [-117.841604, 33.646977],
+          [-117.84149, 33.647129],
+          [-117.841594, 33.64719],
+          [-117.84153, 33.647261],
+          [-117.841428, 33.647202],
+          [-117.841387, 33.647247],
+          [-117.841339, 33.647214]
+        ],
+        "type": "LineString"
+      }
+    }
+  ],
+  "metadata": {
+    "attribution": "openrouteservice.org | OpenStreetMap contributors",
+    "service": "routing",
+    "timestamp": 1773676072524,
+    "query": {
+      "coordinates": [
+        [-117.841832908902, 33.6444700729087],
+        [-117.841159, 33.647176]
+      ],
+      "profile": "foot-walking",
+      "profileName": "foot-walking",
+      "format": "json"
+    },
+    "engine": {
+      "version": "9.5.0",
+      "build_date": "2025-10-31T12:33:09Z",
+      "graph_date": "2026-03-02T19:01:20Z",
+      "osm_date": "2026-02-16T01:00:01Z"
+    }
+  }
 };
 
 type NavProps = 
